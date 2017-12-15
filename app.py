80f5864c9887770ed9729c614172f6e1000caeca
John Liu <johnliu@kkbox.com>
2017-12-15 13:19:12 +0800

    Added flask as server.

---
+++ app.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+from base64 import b64encode
+from flask import Flask
+from flask import redirect, url_for, jsonify
+import requests
+import sys
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return redirect(url_for('static', filename='index.html'))
+
+
+@app.route('/token')
+def get_token(methods=['GET']):
+    auth = b64encode(app.config['client_id'].encode('ascii') +
+                     b':' +
+                     app.config['client_secret'].encode('ascii'))
+    resp = requests.post('https://account.kkbox.com/oauth2/token',
+                         headers={'Authorization': 'Basic ' + auth.decode('ascii')},
+                         data={'grant_type': 'client_credentials'})
+    return jsonify(resp.json())
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: python3 app.py CLIENT_ID CLIENT_SECRET")
+        sys.exit(-1)
+    else:
+        app.config['client_id'] = sys.argv[1]
+        app.config['client_secret'] = sys.argv[2]
+        app.run(port=8888, debug=True)
