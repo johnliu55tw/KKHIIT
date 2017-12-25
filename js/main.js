14e685a616c9e896d592cbfd3d09a61a93f10c8a
John Liu <johnliu@kkbox.com>
2017-12-25 18:45:59 +0800

    Auto-played the KKBOX widget when the HIIT-Timer started.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,26 @@ var PlaylistWidget = require('./playlist_widget/fsm.js').PlaylistWidget
 function main () {
   var sm = new HiitTimerWidget()
 
-  document.getElementById('start-btn').onclick = () => sm.startBtnClicked()
+  document.getElementById('start-btn').onclick = () => {
+    if (sm.state === 'initialized') {
+      // Appending 'autoplay=true' query parameter to src of the widget, for
+      // auto-playing the music while the user click the start button.
+      var widget = document.getElementById('kkbox-widget')
+      widget.src = widget.src + '&autoplay=true'
+    }
+    sm.startBtnClicked()
+  }
   document.getElementById('setting-btn').onclick = () => sm.settingBtnClicked()
-  document.getElementById('reset-btn').onclick = () => sm.resetBtnClicked()
+  document.getElementById('reset-btn').onclick = () => {
+    // Removing the 'autoplay' query parameter
+    var widget = document.getElementById('kkbox-widget')
+    var foundIdx = widget.src.indexOf('&autoplay')
+    if (foundIdx >= 0) {
+      widget.src = widget.src.substr(0, widget.src.indexOf('&autoplay'))
+    }
+
+    sm.resetBtnClicked()
+  }
   document.querySelectorAll('.setting input').forEach((input) => {
     input.oninput = () => sm.settingsUpdated()
     input.onblur = () => sm.settingsUpdated()
